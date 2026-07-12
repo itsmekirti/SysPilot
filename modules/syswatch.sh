867c470dd06089e21a97bb1d238892d012d7252e
Kirti Garg <kirti.growingg@gmail.com>
2026-07-12 11:38:37 +0000

    create log_file variable; add echo in syswatch.sh & check syswatch.log

--- modules/syswatch.sh
+++ modules/syswatch.sh
@@ -3,10 +3,14 @@
 DISK_USAGE=$(df -h | grep dev | awk '{print $5}')
 DISK_USAGE=${DISK_USAGE%\%}
 DISK_THRESHOLD=80
+LOG_FILE=../logs/syswatch.log
 
 if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
 then
     echo "WARN"
+    echo "WARN" >> "$LOG_FILE"
 else
     echo "OK"
+    echo "OK" >> "$LOG_FILE"
 fi
+
