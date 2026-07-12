aab92acd4804db487e0a6ec69beb25eda4eb7f89
Kirti Garg <kirti.growingg@gmail.com>
2026-07-12 12:06:02 +0000

    echo line change

--- modules/syswatch.sh
+++ modules/syswatch.sh
@@ -5,14 +5,11 @@ DISK_USAGE=${DISK_USAGE%\%}
 DISK_THRESHOLD=80
 LOG_FILE=../logs/syswatch.log
 CURRENT_DATE=$(date "+%Y-%m-%d %H:%M:%S")
-echo "$CURRENT_DATE"
 
 if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
 then
-    echo "WARN"
-    echo "WARN" >> "$LOG_FILE"
+    echo ""$CURRENT_DATE" [WARN] "$DISK_USAGE%" THRESHOLD "$DISK_THRESHOLD""
 else
-    echo "OK"
-    echo "OK" >> "$LOG_FILE"
+    echo ""$CURRENT_DATE" [WARN] "$DISK_USAGE%" THRESHOLD "$DISK_THRESHOLD"" >> "$LOG_FILE"
 fi
 
