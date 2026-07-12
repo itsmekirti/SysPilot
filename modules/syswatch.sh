e879baa1d840f3f99a9d329fa458a67ebfe9011c
Kirti Garg <kirti.growingg@gmail.com>
2026-07-12 11:15:58 +0000

    add disk_threshold variable

--- modules/syswatch.sh
+++ modules/syswatch.sh
@@ -2,8 +2,9 @@
 
 DISK_USAGE=$(df -h | grep dev | awk '{print $5}')
 DISK_USAGE=${DISK_USAGE%\%}
+DISK_THRESHOLD=80
 
-if [ $DISK_USAGE -gt 80 ]
+if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
 then
     echo "WARN"
 else
