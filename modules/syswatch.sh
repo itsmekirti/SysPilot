6923921917de31c9e27b57c621ef5f1b86855b67
Kirti Garg <kirti.growingg@gmail.com>
2026-07-12 12:28:17 +0000

    create function check_disk_usage:

--- modules/syswatch.sh
+++ modules/syswatch.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+check_disk_usage(){
+    DISK_USAGE=$(df -h | grep dev | awk '{print $5}')
+    DISK_USAGE=${DISK_USAGE%\%}
+    DISK_THRESHOLD=80
+    LOG_FILE=../logs/syswatch.log
+    CURRENT_DATE=$(date "+%Y-%m-%d %H:%M:%S")
 
-DISK_USAGE=$(df -h | grep dev | awk '{print $5}')
-DISK_USAGE=${DISK_USAGE%\%}
-DISK_THRESHOLD=80
-LOG_FILE=../logs/syswatch.log
-CURRENT_DATE=$(date "+%Y-%m-%d %H:%M:%S")
+    if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
+    then
+        echo ""$CURRENT_DATE" [WARN] "$DISK_USAGE%" THRESHOLD "$DISK_THRESHOLD"" >> "$LOG_FILE"
+    else
+        echo ""$CURRENT_DATE" [OK] "$DISK_USAGE%" THRESHOLD "$DISK_THRESHOLD"" >> "$LOG_FILE"
+    fi
+}
+check_disk_usage
 
-if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
-then
-    echo ""$CURRENT_DATE" [WARN] "$DISK_USAGE%" THRESHOLD "$DISK_THRESHOLD"" >> "$LOG_FILE"
-else
-    echo ""$CURRENT_DATE" [OK] "$DISK_USAGE%" THRESHOLD "$DISK_THRESHOLD"" >> "$LOG_FILE"
-fi
 
