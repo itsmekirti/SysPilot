803ff6fe883e521cf7719dc442b4be98250f10d6
Kirti Garg <kirti.growingg@gmail.com>
2026-07-12 11:54:16 +0000

    did date format

--- modules/syswatch.sh
+++ modules/syswatch.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -h | grep dev | awk '{print $5}')
 DISK_USAGE=${DISK_USAGE%\%}
 DISK_THRESHOLD=80
 LOG_FILE=../logs/syswatch.log
-CURRENT_DATE=$(date)
+CURRENT_DATE=$(date "+%Y-%m-%d %H:%M:%S")
 echo "$CURRENT_DATE"
 
 if [ $DISK_USAGE -gt $DISK_THRESHOLD ]
