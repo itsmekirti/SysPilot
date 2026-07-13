0714068806b9fa507bb350a2c8ad8a7345c38c84
Kirti Garg <kirti.growingg@gmail.com>
2026-07-13 23:23:46 +0530

    create backup_name(just store name)

--- modules/sysbackup.sh
+++ modules/sysbackup.sh
@@ -3,3 +3,5 @@ echo $BACKUP_TIMESTAMP
 BACKUP_SOURCE=/home/kirti/Projects/SysPilot/test_data
 FOLDER_NAME=$(basename "$BACKUP_SOURCE")
 BACKUP_DEST=/home/kirti/Projects/SysPilot/backups
+BACKUP_NAME=backup-$FOLDER_NAME-$BACKUP_TIMESTAMP
+echo $BACKUP_NAME
