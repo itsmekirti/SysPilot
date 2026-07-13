7584299a967941bf8b1f72d6fac41731bb567cb9
Kirti Garg <kirti.growingg@gmail.com>
2026-07-14 00:19:20 +0530

    check desitnationn & source & write logic of creating & verify backup file

--- modules/sysbackup.sh
+++ modules/sysbackup.sh
@@ -3,11 +3,30 @@ BACKUP_SOURCE=/home/kirti/Projects/SysPilot/test_data
 FOLDER_NAME=$(basename "$BACKUP_SOURCE")
 BACKUP_DEST=/home/kirti/Projects/SysPilot/backups
 BACKUP_NAME=backup-$FOLDER_NAME-$BACKUP_TIMESTAMP.tar.gz
-tar -czf "$BACKUP_DEST/$BACKUP_NAME" "$BACKUP_SOURCE"
 
-if tar -tzf "$BACKUP_DEST/$BACKUP_NAME"
+if [ ! -d "$BACKUP_SOURCE" ]
 then
-    echo "SUCCESS"
+    echo "ERROR: Source directory does not exist."
+    exit 1
+fi
+
+if [ ! -d "$BACKUP_DEST" ]
+then
+    echo "ERROR: Backup destination does not exist."
+    exit 1
+fi
+
+if tar -czf "$BACKUP_DEST/$BACKUP_NAME" "$BACKUP_SOURCE"
+then
+    echo "Backup created successfully."
+
+    if tar -tzf "$BACKUP_DEST/$BACKUP_NAME" > /dev/null
+    then
+        echo "Backup verification successful."
+    else
+        echo "Backup verification failed."
+    fi
+
 else
-    echo "FAIL"
+    echo "Backup creation failed."
 fi
