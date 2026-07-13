d86118240651bdcfa6758773d0aef03474d191d2
Kirti Garg <kirti.growingg@gmail.com>
2026-07-14 00:05:40 +0530

    create backup file & verify & logic for verfication status

--- modules/sysbackup.sh
+++ modules/sysbackup.sh
@@ -3,5 +3,11 @@ BACKUP_SOURCE=/home/kirti/Projects/SysPilot/test_data
 FOLDER_NAME=$(basename "$BACKUP_SOURCE")
 BACKUP_DEST=/home/kirti/Projects/SysPilot/backups
 BACKUP_NAME=backup-$FOLDER_NAME-$BACKUP_TIMESTAMP.tar.gz
-
 tar -czf "$BACKUP_DEST/$BACKUP_NAME" "$BACKUP_SOURCE"
+
+if tar -tzf "$BACKUP_DEST/$BACKUP_NAME"
+then
+    echo "SUCCESS"
+else
+    echo "FAIL"
+fi
