9fcf0baa8a29f143c2ad7892e913e5a5c7e2bffb
Kirti Garg <kirti.growingg@gmail.com>
2026-07-13 23:40:50 +0530

    wrote backup code and take backup

--- modules/sysbackup.sh
+++ modules/sysbackup.sh
@@ -1,7 +1,7 @@
 BACKUP_TIMESTAMP=$(date "+%Y-%m-%d-%H%M")
-echo $BACKUP_TIMESTAMP
 BACKUP_SOURCE=/home/kirti/Projects/SysPilot/test_data
 FOLDER_NAME=$(basename "$BACKUP_SOURCE")
 BACKUP_DEST=/home/kirti/Projects/SysPilot/backups
-BACKUP_NAME=backup-$FOLDER_NAME-$BACKUP_TIMESTAMP
-echo $BACKUP_NAME
+BACKUP_NAME=backup-$FOLDER_NAME-$BACKUP_TIMESTAMP.tar.gz
+
+tar -czf "$BACKUP_DEST/$BACKUP_NAME" "$BACKUP_SOURCE"
