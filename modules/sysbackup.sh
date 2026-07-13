350243cb45bd3e5452b4c19ccc51400e894b1b4c
Kirti Garg <kirti.growingg@gmail.com>
2026-07-13 23:16:04 +0530

    add destination of backup file variable

--- modules/sysbackup.sh
+++ modules/sysbackup.sh
@@ -1,4 +1,5 @@
 BACKUP_TIMESTAMP=$(date "+%Y-%m-%d-%H%M")
 echo $BACKUP_TIMESTAMP
 BACKUP_SOURCE=/home/kirti/Projects/SysPilot/test_data
-FOLDER_NAME=$(basename $BACKUP_SOURCE)
+FOLDER_NAME=$(basename "$BACKUP_SOURCE")
+BACKUP_DEST=/home/kirti/Projects/SysPilot/backups
