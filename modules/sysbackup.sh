8ffa189ed6209b2e418bf38210582c9720a4ef5f
Kirti Garg <kirti.growingg@gmail.com>
2026-07-15 23:14:39 +0530

    write logic of backup & and create function

--- modules/sysbackup.sh
+++ modules/sysbackup.sh
@@ -1,45 +1,100 @@
-BACKUP_TIMESTAMP=$(date "+%Y-%m-%d-%H%M")
-BACKUP_SOURCE=/home/kirti/Projects/SysPilot/test_data
-FOLDER_NAME=$(basename "$BACKUP_SOURCE")
-BACKUP_DEST=/home/kirti/Projects/SysPilot/backups
-BACKUP_NAME=backup-$FOLDER_NAME-$BACKUP_TIMESTAMP.tar.gz
+initialize_variable(){
+    BACKUP_TIMESTAMP=$(date "+%Y-%m-%d-%H%M")
+    BACKUP_SOURCE=/home/kirti/Projects/SysPilot/test_data
+    FOLDER_NAME=$(basename "$BACKUP_SOURCE")
+    BACKUP_DEST=/home/kirti/Projects/SysPilot/backups
+    BACKUP_NAME=backup-$FOLDER_NAME-$BACKUP_TIMESTAMP.tar.gz
+    BACKUP_KEEP=7
+}
 
-#check if backup source available or not
-if [ ! -d "$BACKUP_SOURCE" ]
-then
-    echo "ERROR: Source directory does not exist."
-    exit 1
-fi
-
-#check backup destination is available or not 
-if [ ! -d "$BACKUP_DEST" ]
-then
-    echo "ERROR: Backup destination does not exist."
-    exit 1
-fi
-# create and verify backup 
-if tar -czf "$BACKUP_DEST/$BACKUP_NAME" "$BACKUP_SOURCE"
-then
-    echo "Backup created successfully."
-
-    if tar -tzf "$BACKUP_DEST/$BACKUP_NAME" 
-    then
-        echo "Backup verification successful."
-    else
-        echo "Backup verification failed."
+restore_backup(){
+    if [ "$1" = "--restore" ]
+    then 
+        if [ ! -d "$BACKUP_DEST" ]
+        then 
+            echo "Error: Backup directory doesnt exist"
+            exit 1
+        else
+            if [ ! -f "$BACKUP_DEST/$2" ]
+            then
+                echo "error: Backup file doesnt exist"
+                exit 1
+            else
+                if [ ! -d "$3" ]
+                then
+                    echo "error : target directory doesnt exist"
+                    exit 1
+                else
+                    if tar -xzf "$BACKUP_DEST/$2" -C "$3"
+                    then 
+                        echo "Backup file restored successfully"
+                        exit 0
+                    else
+                        echo "Error: Backup file could not restore successfully"
+                        exit 1
+                    fi
+                fi
+            fi
+        fi
     fi
-else
-    echo "Backup creation failed."
-fi
+}
+check_source_directory(){
+    #check if backup source available or not
+    if [ ! -d "$BACKUP_SOURCE" ]
+    then
+        echo "ERROR: Source directory does not exist."
+        exit 1
+    fi
+}
+check_backup_directory(){
+    #check backup destination is available or not 
+    if [ ! -d "$BACKUP_DEST" ]
+    then
+        echo "ERROR: Backup destination does not exist."
+        exit 1
+    fi
+}
+create_backup(){
+    # create and verify backup 
+    if tar -czf "$BACKUP_DEST/$BACKUP_NAME" "$BACKUP_SOURCE"
+    then
+        echo "Backup created successfully."
+        if tar -tzf "$BACKUP_DEST/$BACKUP_NAME" 
+        then
+            echo "Backup verification successful."
+        else
+            echo "Backup verification failed."
+        fi
+    else
+        echo "Backup creation failed."
+    fi    
+}
 
-OLDEST_BACKUP=$(ls -tr ../backups | head -1)
-BACKUP_KEEP=7
+remove_last_backup(){
+    Total_bacups=$(ls ../backups | wc -l)
+    OLDEST_BACKUP=$(ls -tr ../backups | head -1)
+    if [ $Total_bacups -gt $BACKUP_KEEP ]
+    then
+        rm "$BACKUP_DEST/$OLDEST_BACKUP"
+        echo "Oldest Backup File is removed"
+    else
+        echo "Backup files is less than 7"
+    fi
+}
 
-Total_bacups=$(ls ../backups | wc -l)
-if [ $Total_bacups -gt $BACKUP_KEEP ]
+initialize_variable
+if [ "$1" = "--restore" ]
 then
-    rm "$BACKUP_DEST/$OLDEST_BACKUP"
-    echo "Oldest Backup File is removed"
-else
-    echo "Backup files is less than 7"
+    restore_backup "$2" "$3"
 fi
+check_source_directory
+check_backup_directory
+create_backup
+remove_last_backup
+
+
+            
+
+
+    
+
