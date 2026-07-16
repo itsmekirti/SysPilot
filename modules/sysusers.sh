d368b34e9a47b74c94f8f4681d88ab323f8f35ed
Kirti Garg <kirti.growingg@gmail.com>
2026-07-17 00:21:49 +0530

    create user logic

--- modules/sysusers.sh
+++ modules/sysusers.sh
@@ -1,3 +1,28 @@
-create user{
+create_user(){
+    read -p "Enter username: " USERNAME
 
+    if getent group "$DEFAULT_GROUP" 
+    then 
+        echo "group exist"
+    else
+        echo "Group doesnt exist"
+        sudo groupadd "$DEFAULT_GROUP"     
+    fi
+        
+    if id "$USERNAME"
+    then 
+        echo "user exist"
+    else
+        echo "User doesn't exist"
+        sudo useradd -m -g "$DEFAULT_GROUP" "$USERNAME"
+        if id "$USERNAME"
+        then 
+            echo "user created successfully"
+            sudo passwd "$USERNAME"
+            sudo chage -d 0 "$USERNAME"
+            sudo chage -M 90 "$USERNAME"
+        else
+            echo "user doesnt create. there is some issue"
+        fi
+    fi
 }
