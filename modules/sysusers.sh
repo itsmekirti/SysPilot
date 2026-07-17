c85324ac402f6b613b49beae427b7eecbe196892
Kirti Garg <kirti.growingg@gmail.com>
2026-07-17 17:45:51 +0530

    logic update

--- modules/sysusers.sh
+++ modules/sysusers.sh
@@ -28,16 +28,16 @@ create_user(){
         fi
     fi
 }
-create_user
+
 initialize_variables(){
-    CURRENT_TIMESTAMP=$(date "+Y%-%m-%d -%H:%M:%S")
+    CURRENT_TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
     LOG_FILE="/home/kirti/Projects/SysPilot/logs/sysusers.log"
 }
 disable_user(){
     initialize_variables
     read -p "Enter Username: " USERNAME
 
-    if id "$USERNAME"
+    if id "$USERNAME" >/dev/null 2>&1
     then 
         read -p "Enter Reason: " REASON
         sudo usermod -L "$USERNAME"
@@ -45,13 +45,24 @@ disable_user(){
         if [ "$STATUS" = "L" ]
         then 
             echo "User: $USERNAME locked successfully."
+            echo "$CURRENT_TIMESTAMP | User: $USERNAME | Locked Successfully | Reason: $REASON" >> "$LOG_FILE"
             sudo chage -E 0 "$USERNAME"
-            echo "$CURRENT_TIMESTAMP | User: $USERNAME | Disabled | Reason: $REASON" >> $LOG_FILE
-            echo "Successfully expired the $USERNAME account."
+            if [ $? -eq 0 ]
+            then
+                echo "Account expired successfully."
+                echo "$CURRENT_TIMESTAMP | User: $USERNAME | Disabled Successfully | Reason: $REASON" >> "$LOG_FILE"
+            else
+                echo "Failed to expire account."
+                echo "Error: $CURRENT_TIMESTAMP | User: $USERNAME | Disabled Failed | Reason: $REASON" >> "$LOG_FILE"
+            fi
         else
-            echo "$USERNAME" is not locked succefully. Please try again.
+            echo "$USERNAME is not locked successfully. Please try again."
+            echo "$CURRENT_TIMESTAMP | User: $USERNAME | Locked Failed | Reason: $REASON" >> "$LOG_FILE"
         fi
     else
         echo "Error: $USERNAME doesn't exist."
+        echo "$CURRENT_TIMESTAMP | User: $USERNAME | Disabled Failed | User Not Found >> "$LOG_FILE"
+
     fi
 }
+
