0adb9d04e1e967ebf8c9fe019a5ee9c022c7a695
Kirti Garg <kirti.growingg@gmail.com>
2026-07-18 20:31:42 +0530

    complete module 3 User Lifecycle Manager ()

--- modules/sysusers.sh
+++ modules/sysusers.sh
@@ -1,38 +1,54 @@
+initialize_variables(){
+    source ../config/syspilot.conf
+    CURRENT_TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+    LOG_FILE="/home/kirti/Projects/SysPilot/logs/sysusers.log"
+    AUDIT_TIMESTAMP=$(date "+%Y-%m-%d-%H%M%S")
+    AUDIT_DIR="/home/kirti/Projects/SysPilot/audit"
+    AUDIT_REPORT="/home/kirti/Projects/SysPilot/audit/audit-report-$AUDIT_TIMESTAMP.txt"
+}
+
 create_user(){
+    initialize_variables
     read -p "Enter username: " USERNAME
 
     if getent group "$DEFAULT_GROUP" 
     then 
         echo "group exist"
+        echo "$CURRENT_TIMESTAMP | Group $DEFAULT_GROUP exist" >> "$LOG_FILE"
     else
         echo "Group doesnt exist"
-        sudo groupadd "$DEFAULT_GROUP"     
+        echo "$CURRENT_TIMESTAMP | Group $DEFAULT_GROUP doesn't exist" >> "$LOG_FILE"
+        sudo groupadd "$DEFAULT_GROUP"  
+        echo "$CURRENT_TIMESTAMP | Group $DEFAULT_GROUP create successfully" >> "$LOG_FILE"   
     fi
         
     if id "$USERNAME"
     then 
         echo "user exist"
+        echo "$CURRENT_TIMESTAMP | User: $USERNAME | $USERNAME exist." >> "$LOG_FILE"
     else
         echo "User doesn't exist"
+        echo "$CURRENT_TIMESTAMP | User: $USERNAME | $USERNAME exist." >> "$LOG_FILE"
         sudo useradd -m -g "$DEFAULT_GROUP" "$USERNAME"
         if id "$USERNAME"
         then 
             echo "user $USERNAME created successfully"
+            echo "$CURRENT_TIMESTAMP | User: $USERNAME | $USERNAME create successfully." >> "$LOG_FILE"
             sudo passwd "$USERNAME"
+            echo "$CURRENT_TIMESTAMP | User: $USERNAME | Password set for $USERNAME" >> "$LOG_FILE"
             sudo chage -d 0 "$USERNAME"
-            sudo chage -M 90 "$USERNAME"
             echo Password change required on first login.
+            echo "$CURRENT_TIMESTAMP | User: $USERNAME | Password change required on first login." >> "$LOG_FILE"
+            sudo chage -M 90 "$USERNAME"
+            echo "$CURRENT_TIMESTAMP | User: $USERNAME | Password expires after 90 days." >> "$LOG_FILE"  
             echo Password expires after 90 days.
         else
             echo "user doesnt create. there is some issue"
+            echo "Error: $CURRENT_TIMESTAMP | User: $USERNAME | $USERNAME doesn't exist." >> "$LOG_FILE"
         fi
     fi
 }
 
-initialize_variables(){
-    CURRENT_TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-    LOG_FILE="/home/kirti/Projects/SysPilot/logs/sysusers.log"
-}
 disable_user(){
     initialize_variables
     read -p "Enter Username: " USERNAME
@@ -125,11 +141,9 @@ delete_user() {
     fi
 }
 
+
 audit_mode(){
     initialize_variables
-    AUDIT_TIMESTAMP=$(date "+%Y-%m-%d-%H%M%S")
-    AUDIT_DIR="/home/kirti/Projects/SysPilot/audit"
-    AUDIT_REPORT="/home/kirti/Projects/SysPilot/audit/audit-report-$AUDIT_TIMESTAMP.txt"
     mkdir -p "$AUDIT_DIR"
     touch "$AUDIT_REPORT"
     awk -F: '$2=="" {print $1}' /etc/shadow | while read -r USERNAME
@@ -146,6 +160,31 @@ audit_mode(){
             echo "$CURRENT_TIMESTAMP | User: $USERNAME | Duplicate UID 0 Accounts" >> "$AUDIT_REPORT"
         done
     fi
-    
-
 }
+
+world_Writeable_file(){
+    initialize_variables
+    find /home -type f -perm -002 | while read -r FILE
+    do
+        echo "$CURRENT_TIMESTAMP | File: $FILE | World Writable File" >> "$AUDIT_REPORT"
+    done
+}
+
+echo "1. Create User"
+echo "2. Disable User"
+echo "3. Delete User"
+echo "4. Audit"
+echo "5. World Writable Files"
+echo "6. Exit"
+
+read -p "Choose option: " OPTION
+
+case "$OPTION" in
+    1) create_user ;;
+    2) disable_user ;;
+    3) delete_user ;;
+    4) audit_mode ;;
+    5) world_Writeable_file ;;
+    6) exit ;;
+    *) echo "Invalid option" ;;
+esac
