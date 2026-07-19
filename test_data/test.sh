afd36b49e1e794217da492b514fb0ff89edead8c
Kirti Garg <kirti.growingg@gmail.com>
2026-07-19 23:18:52 +0530

    update

--- test_data/test.sh
+++ test_data/test.sh
@@ -1,3 +1,51 @@
 echo "hello"
 echo "first=$1"
 echo "my name is $2"
+
+check_report_directory() {
+    REPORT_GENERATION_PATH="/var/log/syspilot"
+    REPORT_NAME="report-$(date +%Y-%m-%d).txt"
+    REPORT_FILE="$REPORT_GENERATION_PATH/$REPORT_NAME"
+
+    if [ ! -d "$REPORT_GENERATION_PATH" ]
+    then
+        sudo mkdir -p "$REPORT_GENERATION_PATH"
+    fi
+}
+generate_report(){
+
+    check_report_directory
+
+    {
+
+    echo "========================================="
+    echo "       SysPilot Security Report"
+    echo
+    echo "$(date)"
+    echo
+    echo "========================================="
+    echo
+    echo "===== Failed SSH Analysis ====="
+    echo
+
+    failed_ssh_analysis
+
+    echo
+    echo "========================================="
+    echo
+    echo "===== System Errors Analysis ====="
+    echo
+
+    system_error_analysis
+
+    echo
+    echo "========================================="
+    echo
+    echo "===== Successful Logins ======"
+    echo
+
+    successful_logins
+
+    } | sudo tee "$REPORT_FILE"
+
+}
